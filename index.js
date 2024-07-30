a516f9619eda10374aea4464e15053e5660ba9d7
Oluwanifemi Daramola <76186151+nifedara@users.noreply.github.com>
2024-07-30 04:30:15 +0100

    restores menu to initial version

--- index.js
+++ index.js
@@ -1,52 +1,31 @@
-// import {
-//     createContinueResponse,
-//     createEndResponse,
-//     getCardStatusByPhoneNumber
-//   } from "./ussdServices.cjs";
 
-// const express = require('express');
-// const app = express();
-
-import express from 'express';
-import {
-  createContinueResponse,
-  createEndResponse,
-  getCardStatusByPhoneNumber
-} from "./ussdServices.js";
-
+const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5100;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post('/ussd', async(req, res) => {
+app.post('/ussd', (req, res) => {
     try {
         if (req.body) {
-            
             const { msisdn, network, session, text, shortcode, id } = req.body;
-            const statusByPhone = await getCardStatusByPhoneNumber(msisdn);
 
-            if (statusByPhone && statusByPhone.code === "00") {
-                responseText = `Welcome to the LASRRA USSD service \n\nCARD STATUS: ${statusByPhone.cardStatus}`;
-                response = createEndResponse(msisdn, network, shortcode, responseText);
-            
-            } 
-            else {
-                responseText = `Welcome to the LASRRA USSD service \n\nCARD STATUS: record not found\n0. more options`;
-                response = createContinueResponse(msisdn, network, shortcode, responseText);
+            if (msisdn.startsWith("+23470") || msisdn.startsWith("+23480")){
+                response = `Welcome to the LASRRA USSD service \n\n`;
+            }
+            else{
+                response = `Welcome to the LASRRA USSD service \n\n0. more options`;
             }
 
-            res.status(200).json(response);
-            console.log(req)
-        } else {
-            console.log("yepa")
-            res.status(403).json({ message: "data incomplete" })
+            res.set('Content-Type: text/plain');
+            res.send(response);
         }
     } catch (e) {
         console.log("Error", e)
         return res.status(500).json({ message: e })
     }
 });
+
 
 app.listen(port, console.log(`server running on port ${port}`));
