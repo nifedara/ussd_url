63cb24b0246d178ff32d6f24b454dad8ac981b25
Oluwanifemi Daramola <76186151+nifedara@users.noreply.github.com>
2024-10-07 12:53:51 +0100

    updates menu with the expected response

--- index.js
+++ index.js
@@ -9,22 +9,58 @@ app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
   console.log(`Body: ${JSON.stringify(req.body)}`);
+  console.log(`Response: ${JSON.stringify(res)}`);
   next();
 });
 
 app.post('', (req, res) => {
     try {
         if (req.body) {
-            const { msisdn, network, session, text, shortcode, id } = req.body;
+            const { 
+                // msisdn, network, session, text, shortcode, id 
+                backend: {
+                    cuap: {
+                        body: { code_scheme, msisdn, service_code, ussd_content, ussd_op_type, ussd_version },
+                        header: { command_id, command_length, command_status, receiver_id, sender_id }
+                    }
+                },
+                id: { session, ussd },
+                network,
+                session: {
+                    type: { code },
+                    ui: { name }
+                },
+                shortcode,
+                status: { message },
+                text
+            } = req.body;
 
             if (msisdn.startsWith("+23470") || msisdn.startsWith("+23480") || msisdn.startsWith("080") || msisdn.startsWith("070")){
-                response = `Welcome to the LASRRA USSD service \n\n`;
+                ussdResponse = `Welcome to the LASRRA USSD service \n\n`;
             }
             else{
-                response = `Welcome to the LASRRA USSD service \n\n0. more options`;
+                ussdResponse = `Welcome to the LASRRA USSD service \n\n0. more options`;
             }
 
-            res.send(response);
+            const responseObject = {
+                msisdn: msisdn, 
+                network: network, 
+                op_type: ussd_op_type, 
+                session: {
+                    type: { 
+                        code: 3, 
+                        name: "continue" 
+                    },
+                    ui: { 
+                        code: 1, 
+                        name: "input" 
+                    }
+                },
+                shortcode: "", 
+                text: ussdResponse
+              };
+              
+            res.json(responseObject);
         }
     } catch (e) {
         console.error("Error", e);
